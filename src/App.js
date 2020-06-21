77c177f16f4916f58a79054407c5d84f8c90d52d
phreemason <mr.omarmason@gmail.com>
2020-06-21 03:46:07 -0400

    added react router for navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './App.css';
+import Login from './components/Login'
+import Dashboard from './components/Dashboard'
+import { BrowserRouter as Router, Switch, Route, } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/dashboard">
+          <Dashboard />
+        </Route>
+        <Route path="/">
+          <Login />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
